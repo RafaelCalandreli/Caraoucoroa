7c20771de2e67cc115ef0243a040b72ca8303213
RafaelCalandreli <rafaelc_jp_21@hotmail.com>
2017-01-03 20:53:55 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# coroaoucara
+# caraoucoroa
---
+++ app/src/main/java/abbraconsultoria/com/br/coroaoucara/DetalhesActivity.java
@@ -0,0 +1,13 @@
+package abbraconsultoria.com.br.coroaoucara;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalhesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes);
+    }
+}
